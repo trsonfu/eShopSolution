dc8e0183d119e836c721e3f23d55c0fbc06e0eca
trsonfu <trsonfu@gmail.com>
2023-11-27 17:48:35 +0700

    DB Update

---
+++ eShopSolution.Data/Configurations/CatogoryConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    internal class CatogoryConfiguration
+    {
+    }
+}
--- eShopSolution.Data/Entities/Cart.cs
+++ eShopSolution.Data/Entities/Cart.cs
@@ -10,11 +10,8 @@ namespace eShopSolution.Data.Entities
         public int ProductId { set; get; }
         public int Quantity { set; get; }
         public decimal Price { set; get; }
-
         public Guid UserId { get; set; }
-
         public Product Product { get; set; }
-
         public DateTime DateCreated { get; set; }
     }
 }
